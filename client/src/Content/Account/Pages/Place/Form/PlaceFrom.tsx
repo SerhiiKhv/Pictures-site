abb82bcde34d3e3081d0b0bf4ca840e6e4aa0e17
Alamay <khvechuksergiyko@gmail.com>
2023-07-21 23:35:51 +0300

    Update Photos function (remove and select main photo)

--- client/src/Content/Account/Pages/Place/Form/PlaceFrom.tsx
+++ client/src/Content/Account/Pages/Place/Form/PlaceFrom.tsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import {Perks} from "./Perks";
 import axios from "axios";
 import {useParams} from "react-router-dom";
+import {Photos} from "./Photos";
 
 export const PlaceFrom = () => {
     const {id} = useParams()
@@ -9,7 +10,6 @@ export const PlaceFrom = () => {
     const [title, setTitle] = useState('')
     const [address, setAddress] = useState('')
     const [photos, setPhotos] = useState([])
-    const [photoLink, setPhotoLink] = useState('')
     const [description, setDescription] = useState('')
     const [perks, setPerks] = useState([])
     const [extraInfo, setExtraInfo] = useState('')
@@ -74,32 +74,6 @@ export const PlaceFrom = () => {
         </>
     }
 
-    async function addPhotoByLink(e) {
-        e.preventDefault()
-        const {data: filename} = await axios.post('/upload-by-link', {link: photoLink})
-        setPhotos(prev => [...prev, filename])
-        setPhotoLink('')
-    }
-
-    function uploadPhotos(e) {
-        const files = e.target.files
-        console.log({files})
-        const data = new FormData()
-
-        for (let i = 0; i < files.length; i++) {
-            data.append('photos', files[i])
-        }
-
-        axios.post('/upload', data, {
-            headers: {'Content-type': 'multipart/form-data'}
-        }).then(res => {
-            const {data: filenames} = res
-            setPhotos(prev => {
-                return [...prev, ...filenames]
-            })
-        })
-    }
-
     return (
         <>
             <form className="p-2" onSubmit={submit}>
@@ -109,35 +83,7 @@ export const PlaceFrom = () => {
                 {preInput("Address", "Address to this place", "address", setAddress, address)}
 
                 {inputHeader("Photos", "more=better")}
-                <div className="flex gap-2">
-                    <input type={"text"}
-                           value={photoLink}
-                           onChange={e => setPhotoLink(e.target.value)}
-                           placeholder={"Add using a link ...jpg"}
-                           className="p-2 w-full border rounded-2xl border border-gray-400"/>
-                    <button onClick={addPhotoByLink} className="bg-gray-200 px-6 rounded-2xl">Add&nbsp;photo
-                    </button>
-                </div>
-
-                <div className="mt-2 gap-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-                    {photos.length > 0 && photos.map(link => (
-                        <div className="h-32 flex" key={link}>
-                            <img className="rounded-2xl w-full object-cover"
-                                 src={'http://localhost:4000/uploads/' + link} alt={"Loading..."}/>
-                        </div>
-                    ))}
-
-                    <label className="border border-gray-400 bg-transporter items-center
-                        rounded-2xl p-8 text-2xl text-gray-600 flex flex-inline justify-center">
-                        <input type="file" className="hidden" onChange={uploadPhotos}/>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                             stroke="currentColor" className="w-8 h-8">
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                  d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"/>
-                        </svg>
-                        Upload
-                    </label>
-                </div>
+                <Photos photos={photos} setPhotos={setPhotos}/>
 
                 {inputHeader("Description", "Description on the place")}
                 <textarea value={description} onChange={e => setDescription(e.target.value)}
--- client/src/Content/Account/Pages/Place/PlaceList.tsx
+++ client/src/Content/Account/Pages/Place/PlaceList.tsx
@@ -17,15 +17,13 @@ export const PlaceList = () => {
                 <Link to={"/account/places/new/" + place._id} className="cursor-pointer bg-gray-200 gap-2 flex rounded-2xl p-4">
                     <div className="flex w-32 h-32 shrink-0">
                         <img src={"http://localhost:4000/uploads" + place.photos[0]}
-                             className="rounded-2xl" alt="Loading..."/>
+                             className="rounded-2xl w-full" alt="Loading..."/>
                     </div>
                     <div className='grow-0 shrink'>
                         <h2 className="text-xl">{place.title}</h2>
                         <p className="text-sm mt-2">{place.description}</p>
                     </div>
-
                 </Link>
-
             )) :
                 <div>You not have place</div>}
         </div>
